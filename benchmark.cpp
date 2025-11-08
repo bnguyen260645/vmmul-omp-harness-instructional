0d60c253f63743ad0071da6f8b4675d5945a6549
bennguyen0 <bnguyen684@student.fuhsd.org>
2025-11-08 01:32:12 -0800

    timer

--- benchmark.cpp
+++ benchmark.cpp
@@ -100,9 +100,8 @@ int main(int argc, char** argv)
 
         // insert end timer code here, and print out the elapsed time for this problem size
         std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
-        long long elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-        std::cout << " Elapsed time is : " << (elapsed / 10.0) << " seconds" << std::endl;
-
+        std::chrono::duration<double> elapsed = end_time - start_time;
+        std::cout << " Elapsed time is : " << elapsed.count() << " " << std::endl;
 
         // now invoke the cblas method to compute the matrix-vector multiplye
         reference_dgemv(n, Acopy, Xcopy, Ycopy);
